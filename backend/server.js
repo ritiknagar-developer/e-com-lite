27e9c2f161fcae1dac109be86820eeb1c75172ae
ritiknagar-developer <iamritiknagar@gmail.com>
2026-02-20 20:47:29 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,20 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+const allowedOrigins = [process.env.CLIENT_URL_LOCAL, process.env.CLIENT_URL];
 
-app.use(cors());
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
